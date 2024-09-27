1dc8e83004b2b5180ef70536e69eb67966c8282b
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-09-26 22:42:06 -0400

    API installed and working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function poemGenerator(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "roses are red",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function poemGenerator(event) {
+  event.preventDefault();
+
+  let inputInstruction = document.querySelector("#input-instruction");
+  let apiKey = "o7aa001199a3fa308a4b424t253e2953";
+  let context =
+    "You are an expert romantic Haiku writer. Your mission is to generate a haiku poem in basic HTML and separate each line with <br />. Make sure to follow user instructions. Please do not title the poem or add any other marks like '' or words like 'html'. Sign the poem with 'Made with ❤️ by AI' inside a <strong> element only at the end of the poem.";
+  let prompt = `User instructions: Generate a haiku poem about${inputInstruction.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
